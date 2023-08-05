b2b193d31e8d351724597a67196c484a8b81d085
altaf <altaf98khan66@gmail.com>
2023-08-05 16:57:00 +0530

    Add: To append LL 56,30 and 70.

--- src/com/bridgelabz/linkedlist/LLMain.java
+++ src/com/bridgelabz/linkedlist/LLMain.java
@@ -3,9 +3,9 @@ package com.bridgelabz.linkedlist;
 public class LLMain {
     public static void main(String[] args) {
         LL ll = new LL();
-        ll.addFirst("70");
-        ll.addFirst("30");
         ll.addFirst("56");
+        ll.addLast("30");
+        ll.addLast("70");
         ll.printList();
     }
 }
