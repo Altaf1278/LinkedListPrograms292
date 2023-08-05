1b1ba82f8da68cd39e3f3bd6460417f994b8f055
altaf <altaf98khan66@gmail.com>
2023-08-05 16:48:09 +0530

    Add: To create simple LL.

---
+++ src/com/bridgelabz/linkedlist/LLMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linkedlist;
+
+public class LLMain {
+    public static void main(String[] args) {
+        LL ll = new LL();
+        ll.addFirst("70");
+        ll.addFirst("30");
+        ll.addFirst("56");
+        ll.printList();
+    }
+}
